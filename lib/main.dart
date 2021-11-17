309a5574d31993e3b59a14eb4885e88af8a04585
riduan <mohammadriduan39@gmail.com>
2021-11-17 13:36:35 +0600

    testtttttttt

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+      ),
       debugShowCheckedModeBanner: false,
       home: swich(),
     );
--- lib/r-switch.dart
+++ lib/r-switch.dart
@@ -14,23 +14,53 @@ class _swichState extends State<swich> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 200,
-            width: 300,
-            color: _ridu == false ? Colors.limeAccent : Colors.lightGreen,
-            child: Switch(
-              value: _ridu,
-              onChanged: (riduan) {
-                setState(() {
-                  _ridu = riduan;
-                  print(_ridu);
-                });
-              },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 200,
+              width: 300,
+              color: _ridu == false ? Colors.limeAccent : Colors.lightGreen,
+              child: Switch(
+                value: _ridu,
+                onChanged: (riduan) {
+                  setState(() {
+                    _ridu = riduan;
+                    print(_ridu);
+                  });
+                },
+              ),
             ),
-          )
-        ],
+            Divider(
+              color: Colors.black,
+            ),
+            ExpansionTile(
+              title: Text("Riduan"),
+              subtitle: Text("01788337621",style: TextStyle(color: Colors.white),),
+              leading: Icon(Icons.people,color: Colors.white,),
+              trailing: Icon(Icons.more,color: Colors.white,),
+              backgroundColor: Colors.red,
+              children: [
+                Container(
+                  height: 100,
+                  color: Colors.cyan,
+                  child: Row(
+                    children: [
+                      IconButton(onPressed: (){}, icon: Icon(Icons.home)),
+                      Spacer(),
+                      IconButton(onPressed: (){}, icon: Icon(Icons.message)),
+                      Spacer(),
+                      IconButton(onPressed: (){}, icon: Icon(Icons.people)),
+                      Spacer(),
+                      IconButton(onPressed: (){}, icon: Icon(Icons.favorite)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     ));
   }
