46b309d98a7244034e04f28778d3a31e6bb7c837
riduan <mohammadriduan39@gmail.com>
2021-11-17 12:29:15 +0600

    testtt

---
+++ lib/dissmiss.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class disssmisss extends StatefulWidget {
+  const disssmisss({Key? key}) : super(key: key);
+
+  @override
+  _disssmisssState createState() => _disssmisssState();
+}
+
+class _disssmisssState extends State<disssmisss> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
